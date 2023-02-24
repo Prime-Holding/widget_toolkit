932fb8046a8baa9563bbb3d0a8773b8cb6bbf9f1
Georgi Stanev <georgi.stanev@gtmhub.com>
2023-02-24 21:47:06 +0200

    Fix tests

--- test/flutter_test_config.dart
+++ test/flutter_test_config.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:widget_toolkit/shimmer.dart';
+
+import 'helpers/widget_toolkit_file_comparator.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
     GoldenToolkit.runWithConfiguration(
@@ -13,6 +16,12 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
         return testMain();
       },
       config: GoldenToolkitConfiguration(
-        enableRealShadows: true,
-      ),
+          enableRealShadows: true,
+          fileNameFactory: (String name) {
+            final fileName = 'goldens/$name.png';
+
+            goldenFileComparator = WidgetToolkitFileComparator(fileName);
+
+            return fileName;
+          }),
     );
