4c6b8130aa59bb8b5c44ba050a18344e8092b9da
snezhana.romanova <snezhana.romanova@primeholding.com>
2023-03-06 22:48:56 +0200

    Rearrange classes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -44,13 +44,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late PageController pageController;
-
-  @override
-  void initState() {
-    pageController = PageController();
-    super.initState();
-  }
+  final PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
@@ -77,7 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-//# region Common components
+//# region Showcase Pages
 class CommonComponentsPage extends StatelessWidget {
   const CommonComponentsPage({Key? key}) : super(key: key);
 
@@ -217,50 +211,6 @@ class CommonComponentsPage extends StatelessWidget {
       );
 }
 
-class LoadingStateSwitcher extends StatefulWidget {
-  const LoadingStateSwitcher({required this.builder, Key? key})
-      : super(key: key);
-
-  final Widget Function(bool isDataLoading, void Function(bool value)) builder;
-
-  @override
-  State<LoadingStateSwitcher> createState() => _LoadingStateSwitcherState();
-}
-
-class _LoadingStateSwitcherState extends State<LoadingStateSwitcher> {
-  late bool isLoading;
-
-  @override
-  void initState() {
-    _slapAfterThreeSeconds(true);
-    super.initState();
-  }
-
-  Future<void> _slapAfterThreeSeconds(bool initial) async {
-    setState(() {
-      isLoading = initial;
-    });
-    await Future.delayed(const Duration(seconds: 3));
-    setState(() {
-      isLoading = !initial;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) =>
-      widget.builder.call(isLoading, _slapAfterThreeSeconds);
-}
-
-String translateError(BuildContext context, Exception exception) {
-  if (exception is ErrorAccessDeniedModel) {
-    return 'Unable to open link on this device. Perhaps you are missing the'
-        ' right application to open the link.';
-  }
-  return exception.toString();
-}
-//# endregion
-
-//# region Pickers
 class PickersPage extends StatelessWidget {
   const PickersPage({Key? key}) : super(key: key);
 
@@ -329,6 +279,167 @@ class PickersPage extends StatelessWidget {
           ),
         ],
       );
+}
+
+class EditFieldsPage extends StatelessWidget {
+  const EditFieldsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => PageViewNamedPage(
+        title: 'Edit Fields',
+        children: [
+          WidgetSection(
+            description: 'TextFieldDialog',
+            child: TextFieldDialog<String>(
+              errorMapper: (obj, context) =>
+                  ErrorMapperUtil<String>().errorMapper(obj, context),
+              label: 'First Name',
+              value: 'John',
+              validator: LocalAddressFieldService(),
+              dialogHasBottomPadding: true,
+              header: 'Enter your data',
+            ),
+          ),
+          WidgetSection(
+            description: 'EditAddress',
+            child: EditAddressWidget<CountryModel>(
+              cityErrorMapper: (obj, context) =>
+                  ErrorMapperUtil<String>().errorMapper(obj, context),
+              addressErrorMapper: (obj, context) =>
+                  ErrorMapperUtil<String>().errorMapper(obj, context),
+              validator: LocalAddressFieldService(),
+              searchCountryService: SearchService(
+                SearchCountryRepository(),
+              ),
+              editFieldsHaveBottomPadding: true,
+            ),
+          ),
+        ],
+      );
+}
+//# endregion
+
+//# region Helper Methods and Classes
+class PageViewNamedPage extends StatelessWidget {
+  const PageViewNamedPage(
+      {required this.title, required this.children, Key? key})
+      : super(key: key);
+
+  final String title;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) => Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            child: Text(
+              title,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(children: [...children]),
+            ),
+          ),
+        ],
+      );
+}
+
+class WidgetSection extends StatelessWidget {
+  const WidgetSection({
+    required this.child,
+    this.onRefresh,
+    required this.description,
+    this.childSize,
+    super.key,
+  });
+  final Widget child;
+  final String description;
+  final Size? childSize;
+  final void Function()? onRefresh;
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+        child: Card(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      description,
+                      style: const TextStyle(fontSize: 14),
+                    ),
+                    if (onRefresh != null)
+                      IconButton(
+                          onPressed: onRefresh,
+                          icon: const Icon(Icons.refresh)),
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                childSize != null
+                    ? SizedBox(
+                        width: childSize!.width,
+                        height: childSize!.height,
+                        child: child,
+                      )
+                    : child,
+              ],
+            ),
+          ),
+        ),
+      );
+}
+
+class LoadingStateSwitcher extends StatefulWidget {
+  const LoadingStateSwitcher({required this.builder, Key? key})
+      : super(key: key);
+
+  final Widget Function(bool isDataLoading, void Function(bool value)) builder;
+
+  @override
+  State<LoadingStateSwitcher> createState() => _LoadingStateSwitcherState();
+}
+
+class _LoadingStateSwitcherState extends State<LoadingStateSwitcher> {
+  late bool isLoading;
+
+  @override
+  void initState() {
+    _slapAfterThreeSeconds(true);
+    super.initState();
+  }
+
+  Future<void> _slapAfterThreeSeconds(bool initial) async {
+    setState(() {
+      isLoading = initial;
+    });
+    await Future.delayed(const Duration(seconds: 3));
+    setState(() {
+      isLoading = !initial;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      widget.builder.call(isLoading, _slapAfterThreeSeconds);
+}
+
+String translateError(BuildContext context, Exception exception) {
+  if (exception is ErrorAccessDeniedModel) {
+    return 'Unable to open link on this device. Perhaps you are missing the'
+        ' right application to open the link.';
+  }
+  return exception.toString();
 }
 
 class UpdateStateOnSelection<T> extends StatefulWidget {
@@ -452,45 +563,6 @@ class SearchService extends SearchPickerService<CountryModel> {
   List<CountryModel> getPlaceholderList() =>
       List.generate(5, (index) => CountryModel.empty());
 }
-//# endregion
-
-//# region EditFields
-class EditFieldsPage extends StatelessWidget {
-  const EditFieldsPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) => PageViewNamedPage(
-        title: 'Edit Fields',
-        children: [
-          WidgetSection(
-            description: 'TextFieldDialog',
-            child: TextFieldDialog<String>(
-              errorMapper: (obj, context) =>
-                  ErrorMapperUtil<String>().errorMapper(obj, context),
-              label: 'First Name',
-              value: 'John',
-              validator: LocalAddressFieldService(),
-              dialogHasBottomPadding: true,
-              header: 'Enter your data',
-            ),
-          ),
-          WidgetSection(
-            description: 'EditAddress',
-            child: EditAddressWidget<CountryModel>(
-              cityErrorMapper: (obj, context) =>
-                  ErrorMapperUtil<String>().errorMapper(obj, context),
-              addressErrorMapper: (obj, context) =>
-                  ErrorMapperUtil<String>().errorMapper(obj, context),
-              validator: LocalAddressFieldService(),
-              searchCountryService: SearchService(
-                SearchCountryRepository(),
-              ),
-              editFieldsHaveBottomPadding: true,
-            ),
-          ),
-        ],
-      );
-}
 
 class ErrorMapperUtil<T> {
   RxFieldException<T> errorMapper(Object error, BuildContext context) {
@@ -562,88 +634,5 @@ class SearchCountryRepository {
       );
 
   final _countriesList = ['Angola', 'Bulgaria', 'Cuba', 'Egypt', 'Italy'];
-}
-//# endregion
-
-//# region Helper Methods and Classes
-
-class PageViewNamedPage extends StatelessWidget {
-  const PageViewNamedPage(
-      {required this.title, required this.children, Key? key})
-      : super(key: key);
-
-  final String title;
-  final List<Widget> children;
-
-  @override
-  Widget build(BuildContext context) => Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            child: Text(
-              title,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(children: [...children]),
-            ),
-          ),
-        ],
-      );
-}
-
-class WidgetSection extends StatelessWidget {
-  const WidgetSection({
-    required this.child,
-    this.onRefresh,
-    required this.description,
-    this.childSize,
-    super.key,
-  });
-  final Widget child;
-  final String description;
-  final Size? childSize;
-  final void Function()? onRefresh;
-
-  @override
-  Widget build(BuildContext context) => Padding(
-        padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      description,
-                      style: const TextStyle(fontSize: 14),
-                    ),
-                    if (onRefresh != null)
-                      IconButton(
-                          onPressed: onRefresh,
-                          icon: const Icon(Icons.refresh)),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                  width: MediaQuery.of(context).size.width,
-                ),
-                childSize != null
-                    ? SizedBox(
-                        width: childSize!.width,
-                        height: childSize!.height,
-                        child: child,
-                      )
-                    : child,
-              ],
-            ),
-          ),
-        ),
-      );
 }
 //# endregion
