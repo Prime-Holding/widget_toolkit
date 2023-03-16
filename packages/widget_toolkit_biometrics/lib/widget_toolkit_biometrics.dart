71a2ae0fa5a8257965d86915e8d403310256ea23
Toncho Zhelev <toncho.zhelev@primeholding.com>
2023-03-16 15:35:38 +0200

    export only necessary parts of the library

--- packages/widget_toolkit_biometrics/lib/widget_toolkit_biometrics.dart
+++ packages/widget_toolkit_biometrics/lib/widget_toolkit_biometrics.dart
@@ -2,6 +2,5 @@ library widget_toolkit_biometrics;
 
 export 'package:widget_toolkit_biometrics/src/base/data_sources/biometrics_local_data_source.dart';
 export 'package:widget_toolkit_biometrics/src/base/models/biometrics_authentication_type.dart';
-export 'package:widget_toolkit_biometrics/src/base/services/biometrics_service.dart';
-export 'package:widget_toolkit_biometrics/src/lib_biometrics/di/app_level_biometrics_dependencies.dart';
+export 'package:widget_toolkit_biometrics/src/base/models/biometrics_setting_message_types.dart';
 export 'package:widget_toolkit_biometrics/src/lib_biometrics/view/biometrics_switch.dart';
