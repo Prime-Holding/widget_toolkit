6ea9edde69fd5d697309e5a96f1bc4548952ad98
Toncho Zhelev <toncho.zhelev@primeholding.com>
2023-03-17 11:12:04 +0200

    Add docs to biometrics_auth_data_source.dart

--- packages/widget_toolkit_biometrics/lib/src/data_sources/biometrics_auth_data_source.dart
+++ packages/widget_toolkit_biometrics/lib/src/data_sources/biometrics_auth_data_source.dart
@@ -3,25 +3,47 @@ import 'package:local_auth/local_auth.dart';
 import '../models/biometrics_authentication_type.dart';
 
 /// [BiometricsAuthDataSource] is a wrapper of the plugin [local_auth] and
-/// expose its properties, to be used from [BiometricsService]
+/// expose its properties
 class BiometricsAuthDataSource {
-  final LocalAuthentication auth;
+  final LocalAuthentication localAuthentication;
 
-  BiometricsAuthDataSource({required this.auth});
+  ///You must pass in a [LocalAuthentication] instance to be wrapped
+  BiometricsAuthDataSource({required this.localAuthentication});
 
-  Future<bool> get canCheckBiometrics => auth.canCheckBiometrics;
+  /// Returns true if device is capable of checking biometrics.
+  Future<bool> get canCheckBiometrics => localAuthentication.canCheckBiometrics;
 
-  Future<bool> get isDeviceSupported => auth.isDeviceSupported();
+  /// Returns true if device is capable of checking biometrics or is able to
+  /// fail over to device credentials.
+  Future<bool> get isDeviceSupported => localAuthentication.isDeviceSupported();
 
-  Future<bool> authenticate(String localizedReason) => auth.authenticate(
+  /// Authenticates the user with biometrics available on the device while also
+  /// allowing the user to use device authentication - pin, pattern, passcode.
+  ///
+  /// Returns true if the user successfully authenticated, false otherwise.
+  ///
+  /// [localizedReason] is the message to show to user while prompting them
+  /// for authentication. This is typically along the lines of: 'Authenticate
+  /// to access MyApp.'. This must not be empty.
+  ///
+  /// Throws a [PlatformException] if there were technical problems with local
+  /// authentication (e.g. lack of relevant hardware). This might throw
+  /// [PlatformException] with error code [otherOperatingSystem] on the iOS
+  /// simulator.
+  Future<bool> authenticate(String localizedReason) =>
+      localAuthentication.authenticate(
         localizedReason: localizedReason,
         options: const AuthenticationOptions(biometricOnly: true),
       );
 
-  Future<List<BiometricsAuthType>> get availableBiometrics => auth
-      .getAvailableBiometrics()
-      .then((list) => list.map((e) => mapBiometric(e)).toList());
+  /// Returns a list of enrolled biometrics.
+  Future<List<BiometricsAuthType>> get availableBiometrics =>
+      localAuthentication
+          .getAvailableBiometrics()
+          .then((list) => list.map((e) => mapBiometric(e)).toList());
 
+  ///Maps a [BiometricType] from the [local_auth] library
+  ///to a [BiometricAuthType] from this library
   BiometricsAuthType mapBiometric(BiometricType type) {
     switch (type) {
       case BiometricType.face:
