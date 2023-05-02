2a81be513b3459e83e9437890298980d9bdf4853
Stanislav Petkov <stanislav.petkov@primeholding.com>
2023-05-02 15:54:49 +0300

    Update golden test and add file comparator

--- packages/widget_toolkit_pin/test/flutter_test_config.dart
+++ packages/widget_toolkit_pin/test/flutter_test_config.dart
@@ -1,6 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
+
+import 'helpers/widget_toolkit_pin_file_comparator.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
     GoldenToolkit.runWithConfiguration(
@@ -12,5 +15,12 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
       },
       config: GoldenToolkitConfiguration(
         enableRealShadows: true,
+        fileNameFactory: (String name) {
+          final fileName = 'goldens/$name.png';
+
+          goldenFileComparator = WidgetToolkitPinFileComparator(fileName);
+
+          return fileName;
+        },
       ),
     );
