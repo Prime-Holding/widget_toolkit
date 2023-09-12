168bbec2cd2bfb8370c4c4e3b72cf49d27287bed
Krasimir Petkov <krasimir.petkov@primeholding.com>
2023-09-12 14:19:12 +0300

    Update golden toolkit configuration for pin

--- packages/widget_toolkit_pin/test/flutter_test_config.dart
+++ packages/widget_toolkit_pin/test/flutter_test_config.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
-
-import 'helpers/widget_toolkit_pin_file_comparator.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
     GoldenToolkit.runWithConfiguration(
@@ -15,12 +12,5 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
       },
       config: GoldenToolkitConfiguration(
         enableRealShadows: true,
-        fileNameFactory: (String name) {
-          final fileName = 'goldens/$name.png';
-
-          goldenFileComparator = WidgetToolkitPinFileComparator(fileName);
-
-          return fileName;
-        },
       ),
     );
