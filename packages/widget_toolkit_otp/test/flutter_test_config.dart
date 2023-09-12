3b034101816b072a50fd5804448acf5018b78522
Krasimir Petkov <krasimir.petkov@primeholding.com>
2023-09-12 13:53:45 +0300

    Update golden tests for widget_toolkit_otp

--- packages/widget_toolkit_otp/test/flutter_test_config.dart
+++ packages/widget_toolkit_otp/test/flutter_test_config.dart
@@ -1,10 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
-
-import 'helpers/widget_toolkit_file_comparator.dart';
 
 Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
     GoldenToolkit.runWithConfiguration(
@@ -15,12 +12,6 @@ Future<void> testExecutable(FutureOr<void> Function() testMain) async =>
         return testMain();
       },
       config: GoldenToolkitConfiguration(
-          enableRealShadows: true,
-          fileNameFactory: (String name) {
-            final fileName = 'goldens/$name.png';
-
-            goldenFileComparator = WidgetToolkitFileComparator(fileName);
-
-            return fileName;
-          }),
+        enableRealShadows: true,
+      ),
     );
