2ab17e3a878cbff2bf60c625af9c3a6fd8589615
JvnSlv <jovan.slavkovic@primeholding.com>
2024-07-31 15:18:08 +0200

    Implement all SmallButton golden test into a single image

---
+++ packages/widget_toolkit/test/lib_ui_components/buttons/view/small_button_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:widget_toolkit/ui_components.dart';
+
+import '../../../helpers/golden_helper.dart';
+import '../factory/small_button_factory.dart';
+
+void main() {
+  runGoldenBuilderTests(
+    'SmallButton Golden Tests',
+    surfaceSize: const Size(700, 350),
+    matcherCustomPump: (widget) =>
+        widget.pump(const Duration(milliseconds: 350)),
+    builder: GoldenBuilder.grid(
+      columns: 4,
+      widthToHeightRatio: 1,
+    )
+      ..addScenario(
+          'loading filled',
+          smallButtonFactory(
+              type: SmallButtonType.filled, state: ButtonStateModel.loading))
+      ..addScenario(
+          'enabled filled',
+          smallButtonFactory(
+              type: SmallButtonType.filled, state: ButtonStateModel.enabled))
+      ..addScenario(
+          'disabled filled',
+          smallButtonFactory(
+              type: SmallButtonType.filled, state: ButtonStateModel.disabled))
+      ..addScenario(
+          'pressed filled',
+          smallButtonFactory(
+              type: SmallButtonType.filled, state: ButtonStateModel.pressed))
+      ..addScenario(
+          'loading outlined',
+          smallButtonFactory(
+              type: SmallButtonType.outline, state: ButtonStateModel.loading))
+      ..addScenario(
+          'enabled outlined',
+          smallButtonFactory(
+              type: SmallButtonType.outline, state: ButtonStateModel.enabled))
+      ..addScenario(
+          'disabled outlined',
+          smallButtonFactory(
+              type: SmallButtonType.outline, state: ButtonStateModel.disabled))
+      ..addScenario(
+          'pressed outlined',
+          smallButtonFactory(
+              type: SmallButtonType.outline, state: ButtonStateModel.pressed)),
+  );
+}
